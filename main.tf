c36b4bb96eaec4cee69547b9ff6e0f5d2eb05029
Ravindra Shimpi <ravindra.shimpi@gmail.com>
2022-02-21 15:39:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,6 @@ provider "aws" {
 /**
 * Provision a new EC2 instance
 **/
-//Step1-2
 resource "aws_instance" "ec2-instance" {
   ami                    = "ami-033b95fb8079dc481"
   instance_type          = "t2.micro"
